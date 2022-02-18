3b439e4c291f59c864281644a71ec26caa925c78
Nicole Li <hla110@sfu.ca>
2022-02-17 22:12:05 -0800

    revert ssl setting

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:{
-    rejectUnauthorized:false
-  }
+  connectionString: process.env.DATABASE_URL
 })
 
 var app = express();
